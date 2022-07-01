4339a185b2ea4f5e51c776ec717775a67d77bc8a
idjem <mejdiourir@gmail.com>
2022-07-01 11:24:31 +0200

    fix: typo

--- index.js
+++ index.js
@@ -95,5 +95,5 @@ app.post("/salesforce", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
