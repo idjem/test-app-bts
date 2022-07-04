0908b46e3af818d1a6f2a51f1ba5966b800ca9a4
Madison Deken <madison556@hotmail.fr>
2022-07-04 10:57:16 +0200

    chore: refactors /new-case route for better readability

--- index.js
+++ index.js
@@ -26,53 +26,56 @@ app.post("/new-case", (req, res) => {
   const ticketSolved = inputValues["is-solved"];
 
   if (
-    // initial ticket solved form
-    data.component_id === isTicketSolved.id &&
+    // case is solved
     ticketSolved === ticketSolvedOption.id
   ) {
-    res
-      .status(200)
-      .json(newSolvedCaseForm({ isTicketSolved: ticketSolved, inputValues }));
-  } else if (
+    if (
+      // initial ticket solved form
+      data.component_id === isTicketSolved.id
+    ) {
+      res
+        .status(200)
+        .json(newSolvedCaseForm({ isTicketSolved: ticketSolved, inputValues }));
+    }
     // category selected ticket solved form
-    ticketSolved === ticketSolvedOption.id &&
-    ticketCategory().options.some(
-      (category) => category.id === data.component_id
-    )
-  ) {
-    res.status(200).json(
-      newSolvedCaseForm({
-        isTicketSolved: ticketSolved,
-        categoryId: data.component_id,
-        inputValues,
-      })
-    );
-  } else if (
+    else if (
+      ticketCategory().options.some(
+        (category) => category.id === data.component_id
+      )
+    ) {
+      res.status(200).json(
+        newSolvedCaseForm({
+          isTicketSolved: ticketSolved,
+          categoryId: data.component_id,
+          inputValues,
+        })
+      );
+    }
     // subcategory selected ticket solved form
-    ticketSolved === ticketSolvedOption.id &&
-    ticketSubCategories(inputValues[ticketCategory().id]).options.some(
-      (subCategory) => subCategory.id === data.component_id
-    )
-  ) {
-    const categoryId = inputValues[ticketCategory().id];
-    const subCategoryId = inputValues[ticketSubCategories(categoryId).id];
+    else if (
+      ticketSubCategories(inputValues[ticketCategory().id]).options.some(
+        (subCategory) => subCategory.id === data.component_id
+      )
+    ) {
+      const categoryId = inputValues[ticketCategory().id];
+      const subCategoryId = inputValues[ticketSubCategories(categoryId).id];
 
-    res.status(200).json(
-      newSolvedCaseForm({
-        isTicketSolved: ticketSolved,
-        categoryId: inputValues[ticketCategory().id],
-        subCategoryId,
-        inputValues,
-      })
-    );
-  } else if (
-    // submit ticket solved form
-    ticketSolved === ticketSolvedOption.id &&
-    data.component_id === ticketSolvedSubmit.id
-  ) {
-    console.log("sends solved ticket to salesforce");
-    console.log(inputValues);
-    res.status(200).json({ ok: "ok", values: inputValues });
+      res.status(200).json(
+        newSolvedCaseForm({
+          isTicketSolved: ticketSolved,
+          categoryId: inputValues[ticketCategory().id],
+          subCategoryId,
+          inputValues,
+        })
+      );
+    }
+    // submit the solved case form
+    else if (data.component_id === ticketSolvedSubmit.id) {
+      console.log("sends solved ticket to salesforce");
+      console.log(inputValues);
+      res.status(200).json({ ok: "ok", values: inputValues });
+    }
+    res.status(400);
   } else {
     res.status(200).json(newSolvedCaseForm());
   }
